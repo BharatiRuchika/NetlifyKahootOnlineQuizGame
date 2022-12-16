573c1e1319dc0252c916ea2551c46c4fa7fc6ca3
BharatiRuchika <ruchikabharaticdac@gmail.com>
2022-12-16 12:18:03 +0530

    first commit

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const express = require('express')
     ,bodyParser = require('body-parser')
     ,{Quiz} = require('./utils/quiz')
 
-  
-
-const {
+  const {
     SERVER_PORT,
     SESSION_SECRET,
     DOMAIN,
@@ -22,28 +20,21 @@ const {
     FRONTEND_URL
 } =  process.env;
 
-
-
 var createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addQuizRouter = require('./routes/addQuiz');
 var quizQuestionsRouter = require('./routes/quizQuestions');
 // const http = require("http");
-
 var mongo = require("./connection");
 const { Socket } = require('socket.io');
 mongo.connect();
 
 var app = express();
-// app.use(cors({   origin: true,   credentials: true }))
-// app.use(cors({ credentials: true }));
-// const socket = require('socket.io');
 app.use(function (req, res, next) {
   console.log("environment",process.env.NODE_ENV)
   res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000","https://vercel.com");
@@ -58,11 +49,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// const port = process.env.PORT;
 const  {createServer}  = require("http");
 const {Server} = require("socket.io");
 const httpServer = createServer(app);
-// const srver = require('http').createServer(app)
 const io = new Server(httpServer,{
   cors: {
     origin: true,
@@ -70,80 +59,48 @@ const io = new Server(httpServer,{
     optionSuccessStatus:200
   }}
   );
-// const io = require('socket.io')(server)
-
-// const port =24257;
-
-
 const users = {};
 var clients = {};
 var hosts={};
 var socketArray=[];
 var hostObject={};
 var hostArray=[];
-// let currentConnections = {};
 var host=0;
-// var hostSocketArray = {};
 io.on('connection', socket => {
- 
-  // clients[socket.id] = socket;
  console.log("socketid",socket.id);
-//  currentConnections[socket.id] = { socket: socket };
-    console.log("connection established");
-    // io.sockets.emit('conn')
-    socket.on('host-join', (data) => {
-    host++;
-    console.log("host",host);
-    console.log("socketId",socket.id);
-    // hostObject.hostId = socket.id;
-    hostArray.push(socket.id);
-    console.log("hostArray",hostArray);
-    // hosts[socket.id] = socket.id;
-    // socket["socketid"] = socket.id
-    console.log("host join");
- 
-    console.log("data",data);
-    var selectedPin = data.pin;
-    io.sockets.emit('host-joined',{id:socket.id})
-    // users[socket.id] = data.nickname;
-   socket.join(selectedPin);  
-  })
+  console.log("connection established");
+  socket.on('host-join', (data) => {
+  host++;
+  console.log("host",host);
+  console.log("socketId",socket.id);
+  hostArray.push(socket.id);
+  console.log("hostArray",hostArray);
+  console.log("host join");
+  console.log("data",data);
+  var selectedPin = data.pin;
+  io.sockets.emit('host-joined',{id:socket.id})
+  socket.join(selectedPin);  
+})
   
-  socket.on('player-joined', (data) => {
-  // users[socket.id] = data.nickname;
-  // onlineUsers[username] = socket.id;
-  users[socket.id] = socket.id;
-  // socket["username"] = username;
-  // socket["socketid"] = socket.id;
+    socket.on('player-joined', (data) => {
+    users[socket.id] = socket.id;
     console.log("player joined");
     console.log("data",data);
-   console.log("player",socket.id);
-  var nickname = data.nickname;
-  console.log("nickname",nickname);
-  var selectedPin = data.selectedPin;
-  console.log("SelectedPin",selectedPin);
-  
+    console.log("player",socket.id);
+    var nickname = data.nickname;
+    console.log("nickname",nickname);
+    var selectedPin = data.selectedPin;
+    console.log("SelectedPin",selectedPin);
     socket.join(selectedPin);
-   
-    
   })
-  //Add player to Quiz Object
- 
-
-socket.on('player-add', (data)=> {
+ socket.on('player-add', (data)=> {
   console.log("player add");
-  // console.log("socket",socket);
-  // const { error, user } = addUser(
-  //   { id: socket.id, name:data.nickname, room :data.selectedPin});
   var pin = parseInt(data.selectedPin);
   console.log("selectedpin",typeof pin);
   console.log("player_data",data.nickname);
   console.log("player_data",socket.id);
- io.sockets.emit("hello");
-//  socket.to(`${data.selectedPin}`).emit('room-joined', {name: data.nickname, id: socket.id});
- io.sockets.emit('room-joined', {name: data.nickname, id: socket.id,pin:data.selectedPin});
-  
-  
+  io.sockets.emit("hello");
+  io.sockets.emit('room-joined', {name: data.nickname, id: socket.id,pin:data.selectedPin});
 })
 
   socket.on('question-over', (data) => {
@@ -186,15 +143,11 @@ socket.on('destroy', function (data) {
   console.log("data",data);
   console.log('A user disconnected');
   io.sockets.emit('left', {id:socket.id});
-  // delete currentConnections[socket.id];
   socket.leave(data); 
-  // socket.disconnect();
 });
 socket.on("pin-entered",(pin)=>{
   console.log("pin entered called");
   console.log(hostArray);
-  // console.log(hostArray["hostsocketid"]);
-  // var id = socketArray["socketid"];
   console.log("pin",pin);
   socketArray.push(socket.id);
   var id = socketArray[0];
@@ -239,21 +192,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use((err,req,res,next)=>{
-//   next();
-// })
-// app.get('/', (req, res) => {
-//   // res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-//   res.send("working")
-// })
 
-if(process.env.NODE_ENV=="production"){
-  const path = require("path");
-  app.get("/",(req,res)=>{
-    app.use(express.static(path.resolve(__dirname,'client','build','index.html')))
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-  })
-}
+
+app.get('/',(req,res)=>{
+  console.log(res.send("App is working fine"));
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use((req,res,next)=>{
@@ -304,8 +247,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
